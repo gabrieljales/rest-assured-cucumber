96ea876673c1adbabff24b3d1decfd0e4cf326dd
gabrieljales <gabrielnascimentojaless@gmail.com>
2025-06-28 21:01:22 -0300

    feat: Add NoArgsConstructor

--- src/test/java/api/jsonplaceholder/domain/models/Post.java
+++ src/test/java/api/jsonplaceholder/domain/models/Post.java
@@ -3,9 +3,11 @@ package api.jsonplaceholder.domain.models;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Post {
     private int userId;
